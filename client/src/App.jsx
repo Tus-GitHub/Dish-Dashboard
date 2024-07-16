56e690e644f2838fca1032b8363eeeb1d5aa4b9e
Tus-GitHub <tushardhull22@gmail.com>
2024-07-16 18:45:54 +0530

    complete Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Header from './components/Header'
+import About from './pages/About'
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
     <Header />
       <Routes>
         <Route path = "/" element ={<Dashboard />} />
+        <Route path = "/about" element ={<About />} />
       </Routes>
     </BrowserRouter>
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header className='bg-slate-200 shadow-md'>
+        <div className='flex justify-between items-center max-w-8xl mx-auto p-3'>
+            <Link to="/">
+            <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+                <span className='text-slate-500'>DISH-</span>
+                <span className='text-slate-700'>DASHBOARD</span>
+            </h1>
+            </Link>
+            <ul className="flex gap-4">
+                <Link to ="/">
+                <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
+                </Link>
+                <Link to="/about">
+                <li className="hidden sm:inline text-slate-700 hover:underline">About</li>
+                </Link>
+            </ul>
+        </div>
+    </header>
+
   )
 }
